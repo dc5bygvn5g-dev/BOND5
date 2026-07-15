8f5a4c4e57ab5907c979aa66db0762b44f4db825
dc5bygvn5g-dev <dc5bygvn5g@privaterelay.appleid.com>
2026-07-15 19:58:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,8 @@
-window.addEventListener("load", () => {
+window.onload = () => {
+    const loader = document.getElementById("loader");
 
-const loader = document.getElementById("loader");
-
-setTimeout(() => {
-
-loader.style.opacity = "0";
-loader.style.pointerEvents = "none";
-
-setTimeout(() => {
-
-loader.style.display = "none";
-
-},1000);
-
-},2200);
-
-});
-
-const hero = document.querySelector(".hero");
-
-document.addEventListener("mousemove",(e)=>{
-
-const x = (e.clientX / window.innerWidth - .5) * 20;
-
-const y = (e.clientY / window.innerHeight - .5) * 20;
-
-hero.style.transform =
-`translate(${x}px,${y}px)`;
-
-});
-
-const observer = new IntersectionObserver((entries)=>{
-
-entries.forEach(entry=>{
-
-if(entry.isIntersecting){
-
-entry.target.classList.add("show");
-
-}
-
-});
-
-});
-
-document.querySelectorAll("section").forEach(section=>{
-
-observer.observe(section);
-
-});
-
-document.querySelectorAll("a[href^='#']").forEach(anchor=>{
-
-anchor.addEventListener("click",function(e){
-
-e.preventDefault();
-
-document.querySelector(this.getAttribute("href")).scrollIntoView({
-
-behavior:"smooth"
-
-});
-
-});
-
-});
+    setTimeout(() => {
+        loader.style.opacity = "0";
+        loader.style.visibility = "hidden";
+    }, 2000);
+};
